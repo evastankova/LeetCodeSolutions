b6e16ff89ab865cc022b2919086022ff9a3d2152
go69bah <eva.stankova@tum.de>
2024-10-26 18:35:24 +0200

    task roman to int upload

---
+++ src/RomanToInt.java
@@ -0,0 +1,66 @@
+public class RomanToInt {
+    public int romanToInt(String s) {
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'M') {
+                result += 1000;
+            } else if (s.charAt(i) == 'D') {
+                result += 500;
+            } else if (s.charAt(i) == 'C') {
+
+                if (i + 1 < s.length()) {
+                    if (s.charAt(i + 1) == 'D') {
+                        result += 400;
+                        i++;
+                    } else if (s.charAt(i + 1) == 'M') {
+                        result += 900;
+                        i++;
+                    } else {
+                        result += 100;
+                    }
+                } else {
+                    result += 100;
+                }
+
+            } else if (s.charAt(i) == 'L') {
+                result += 50;
+            } else if (s.charAt(i) == 'X') {
+
+                if (i + 1 < s.length()) {
+                    if (s.charAt(i + 1) == 'L') {
+                        result += 40;
+                        i++;
+                    } else if (s.charAt(i + 1) == 'C') {
+                        result += 90;
+                        i++;
+                    } else {
+                        result += 10;
+                    }
+                } else {
+                    result += 10;
+                }
+
+            } else if (s.charAt(i) == 'V') {
+                result += 5;
+            } else if (s.charAt(i) == 'I') {
+
+                if (i + 1 < s.length()) {
+                    if (s.charAt(i + 1) == 'V') {
+                        result += 4;
+                        i++;
+                    } else if (s.charAt(i + 1) == 'X') {
+                        result += 9;
+                        i++;
+                    } else {
+                        result += 1;
+                    }
+                } else {
+                    result += 1;
+                }
+            }
+        }
+
+        return result;
+    }
+}
