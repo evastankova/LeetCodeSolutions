5390099ba5c000cac59850f3fb66511b96d1e5a8
go69bah <eva.stankova@tum.de>
2024-12-15 18:57:24 +0100

    task path sum upload

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -14,4 +14,14 @@ public class TreeNode {
         this.left = left;
         this.right = right;
     }
+
+    /* Tree example
+        TreeNode t1 = new TreeNode(2);
+        TreeNode t2 = new TreeNode(4);
+        TreeNode t3 = new TreeNode(3, t1, t2);
+        TreeNode t5 = new TreeNode(8);
+        TreeNode t6 = new TreeNode(10);
+        TreeNode t7 = new TreeNode(9, t5, t6);
+        TreeNode t4 = new TreeNode(1, t3, t7);
+     */
 }
